66681eae4a44ce054231d3a6119f8856d6056a8d
Rodrigo <rodrigo_mdg_fernandes@outlook.com>
2020-01-07 10:56:57 -0300

    Paginação

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -107,6 +107,33 @@ class Category extends Model
 		}
 	}
 
+	public function getProductsPage($page = 1, $itensPerPage = 8)
+	{
+
+		$start = ($page - 1) * $itensPerPage;
+
+		$sql = new Sql();
+
+		$results = $sql->select("
+			SELECT SQL_CALC_FOUND_ROWS *
+			FROM tb_products AS a
+			INNER JOIN tb_productscategories AS b ON a.idproduct = b.idproduct
+			INNER JOIN tb_categories AS c ON c.idcategory = b.idcategory
+			WHERE c.idcategory = :idcategory
+			LIMIT $start,$itensPerPage;",
+			[
+				':idcategory'=>$this->getidcategory()
+			]);
+
+		$resultTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal;");
+
+		return [
+			'data'=>Product::checkList($results),
+			'total'=>$resultTotal[0]["nrtotal"],
+			'pages'=>ceil($resultTotal[0]["nrtotal"] / $itensPerPage)
+		];
+	}
+
 	public function addProduct(Product $product)
 	{
 		$sql = new Sql();
